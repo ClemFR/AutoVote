5b452f97fd7f9406fcc94cbd9109b1d1bef38c1d
BuildTools <clementlaurie27@gmail.com>
2021-03-07 22:22:38 +0100

    Chrome is now invisible

--- Variables.py
+++ Variables.py
@@ -1,8 +1,15 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+WINDOW_SIZE = "1920,1080"
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
 
 
 class MyGlobals(object):
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(chrome_options=chrome_options)
     timer1H = 0
     timer1H30 = 0
     timer2H = 0
